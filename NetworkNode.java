a5e39d17f09ecbed96a745fb94359d54cd2d8b6c
alejo280706 <alejo280706@gmail.com>
2026-08-05 10:09:23 -0500

    Create NetworkNode class with connection method

--- NetworkNode.java
+++ NetworkNode.java
@@ -1,5 +1,16 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class NetworkNode {
+    private String addis;
+    private String port;
+    private String isOpen;
+    private String isConected;
+    public NetworkNode(String addis, String port, String isOpen, String isConected) {
+        this.addis = addis;
+        this.port = port;
+        this.isOpen = isOpen;
+        this.isConected = isConected;
     }
-}
+    public void connect(NetworkNode node){
+        
+    }
+    
+    }
